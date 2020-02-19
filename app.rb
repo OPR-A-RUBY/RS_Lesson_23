e67af5fd4cacf5efd9fdd4c2bb570192911ecdac
OPR_A <opr.a.ruby@gmail.com>
2020-02-19 21:35:13 +0400

    Getting parametrs post query

--- app.rb
+++ app.rb
@@ -16,16 +16,18 @@ get '/visit' do
 end
 
 post '/visit' do
+
 	@user_name = params[:user_name]
     @phone     = params[:phone]
     @date_time = params[:date_time]
+    @barber    = params[:barber]
 
     @title = 'Спасибо!'
     @message = "Спасибо вам, #{@user_name}, будем ждать Вас."
 
     out_f = File.open './public/users.txt', 'a'
     out_f.write "User: #{@user_name}, Phone: #{@phone},"
-    out_f.write " Date_Time: #{@date_time}\n"
+    out_f.write " Date_Time: #{@date_time} Barber: #{@barber}\n"
     out_f.close
 
     erb :message
